0f76da861ea441c462ac159e75614179c8787f69
0811994@rgu.ac.uk <Vakeger123>
2016-04-25 16:28:30 +0100

    test

--- search.php
+++ search.php
@@ -25,7 +25,7 @@ if(isset($_POST["submit"]))
     }
     else
     {
-        $resultText = $searchID + "no user with that username";
+        $resultText = "no user with that username";
 
     }
 }
