5a8e6955890f58610e621b4c1ebc48cc9efe9622
Csaba <0811994@rgu.ac.uk>
2016-04-26 22:10:38 +0100

    protection agains SQLi on main login

--- login.php
+++ login.php
@@ -13,8 +13,9 @@
 			// Define $username and $password
 			$username=$_POST['username'];
 			$password=$_POST['password'];
-
-
+			$username=mysqli_real_escape_string($db, $username);
+			$password=mysqli_real_escape_string($db, $password);
+			$password = md5($password);
 			
 			//Check username and password from database
 			$sql="SELECT userID FROM users WHERE username='$username' and password='$password'";
