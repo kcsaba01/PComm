53ed9a2ee391e88ebcf36621b02bd54b82f64b82
Csaba <0811994@rgu.ac.uk>
2016-04-27 22:07:33 +0100

    protection agains SQLi on main login

--- submit.php
+++ submit.php
@@ -1,6 +1,7 @@
 <?php
 $msg = "";
 include("connection.php");
+include("utilities.php");
 $conn = new mysqli(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
