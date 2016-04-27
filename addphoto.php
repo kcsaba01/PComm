d47eab6cfc12bf5fcc75f5c0117785a7f9866db1
Csaba <0811994@rgu.ac.uk>
2016-04-27 01:52:07 +0100

    protection agains SQLi on main login

--- addphoto.php
+++ addphoto.php
@@ -7,43 +7,39 @@ $title="title1";
 $desc="desc1";
 $target_file="targetfile1";
 $id=1;
-if(isset($_POST["submit"]))
-{
+if(isset($_POST["submit"])) {
     $title = $_POST["title"];
     $desc = $_POST["desc"];
     $url = "test";
     $name = $_SESSION["username"];
     $target_dir = "uploads/";
     $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-    $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+    $imageFileType = pathinfo($target_file, PATHINFO_EXTENSION);
     $uploadOk = 1;
 
-    $sql="SELECT userID FROM users WHERE username='$name'";
+    $sql = "SELECT userID FROM users WHERE username='$name'";
     $result = $conn->query($sql);
-    if ($result->num_rows = 1)
-    {
+    if ($result->num_rows = 1) {
         $id = $row['userID'];
     } else {
         $msg = "You need to login first";
     }
 
-}
-//Binding the parameter, the statement is contained in utilities.php
-if(!($insertphotos->bind_param("ssst",$title,$desc,$target_file,$id)))
-{
-    xecho("Binding has failed" . $insertphotos->errno . " " . $insertphotos->error);
-    xecho($title . $desc . $target . $id);
-}
-//Executing
-if (!$insertphotos->execute())
-{
-    xecho("Execute has failed" . $insertphotos->errno . " " . $insertphotos->error);
-}
-if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
 
-    $msg = "Thank You! The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded. click <a href='photos.php'>here</a> to go back";
-}
-else {
-   $msg = "Sorry, there was an error uploading your file.";
+//Binding the parameter, the statement is contained in utilities.php
+    if (!($insertphotos->bind_param("ssst", $title, $desc, $target_file, $id))) {
+        xecho("Binding has failed" . $insertphotos->errno . " " . $insertphotos->error);
+        xecho($title . $desc . $target . $id);
+    }
+//Executing
+    if (!$insertphotos->execute()) {
+        xecho("Execute has failed" . $insertphotos->errno . " " . $insertphotos->error);
+    }
+    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+
+        $msg = "Thank You! The file " . basename($_FILES["fileToUpload"]["name"]) . " has been uploaded. click <a href='photos.php'>here</a> to go back";
+    } else {
+        $msg = "Sorry, there was an error uploading your file.";
+    }
 }
 ?>
