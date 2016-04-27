f3c903a986c69a1cc1b7d149ada4f489bd441273
Csaba <0811994@rgu.ac.uk>
2016-04-28 00:05:41 +0100

    protection agains SQLi on main login

--- login.php
+++ login.php
@@ -17,29 +17,36 @@
 			$username=mysqli_real_escape_string($db, $username);
 			$password=mysqli_real_escape_string($db, $password);
 			$password = md5($password);
-			
-			//Check username and password from database
-			$sql="SELECT userID, attempt FROM users WHERE username='$username' and password='$password'";
-			$result=mysqli_query($db,$sql);
-			$row=mysqli_fetch_array($result,MYSQLI_ASSOC) ;
-			
-			//If username and password exist in our database then create a session.
-			//Otherwise echo error.
-			
-			if(mysqli_num_rows($result) == 1 and $row[attempt] < 4) //checking whether the user exist and there were less than 4 login attempts
-			{
-				$_SESSION['username'] = $username; // Initializing Session
-				//If login was successful the attempt field is changed to 1
-				$query = mysqli_query($db, "UPDATE users SET attempt=1 WHERE username='$username'") or die(mysqli_error($db));
-				header("location: photos.php"); // Redirecting To Other Page
-			}else
-			{
-				$error = "Incorrect username/password or the acount is blocked";
-				//If the login was unsuccessful the attempt field is incremented with 1
-				$query = mysqli_query($db, "UPDATE users SET attempt=attempt+1 WHERE username='$username'") or die(mysqli_error($db));
-			}
 
+			if ($stmt = mysqli_prepare($db,"SELECT userID, attempt FROM users WHERE username='?' and password='?'")) //Preparing the statement
+			{
+				mysqli_stmt_bind_param($stmt, "ss", $username, $password); //Binding the variables
+				if (mysqli_stmt_execute($stmt))
+				{
+					mysqli_stmt_bind_result($stmt, $result);
+					$row=mysqli_fetch_array($result,MYSQLI_ASSOC) ;
+					if(mysqli_num_rows($result) == 1 and $row[attempt] < 4) //checking whether the user exist and there were less than 4 login attempts
+					{
+						$_SESSION['username'] = $username; // Initializing Session
+						//If login was successful the attempt field is changed to 1
+						$query = mysqli_query($db, "UPDATE users SET attempt=1 WHERE username='$username'") or die(mysqli_error($db));
+						header("location: photos.php"); // Redirecting To Other Page
+					}else
+					{
+						$error = "Incorrect username/password or the acount is blocked";
+						//If the login was unsuccessful the attempt field is incremented with 1
+						$query = mysqli_query($db, "UPDATE users SET attempt=attempt+1 WHERE username='$username'") or die(mysqli_error($db));
+					}
+				}
+				else
+				{
+					$msg =mysqli_stmt_error($stmt) . " There was an error"; //Displaying the reason why the adding has failed
+				}
+
+			}
 		}
+		mysqli_stmt_close($stmt); //closing the statement
+		mysqli_close($db); //closing the connection
 	}
 
 ?>
