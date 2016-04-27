64881d51e8f025435c0d44e4c7346f37cf218543
Csaba <0811994@rgu.ac.uk>
2016-04-27 23:09:00 +0100

    protection agains SQLi on main login

--- connection.php
+++ connection.php
@@ -4,6 +4,10 @@ define('DB_USERNAME', 'bed4ebe251b726');
 define('DB_PASSWORD', '1370829c');
 define('DB_DATABASE', 'pcommdb');
 $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+if (mysqli_connect_errno()) {
+    printf("Connect failed: %s\n", mysqli_connect_error());
+    exit();
+}
 //$conn = new mysqli(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 //if ($conn->connect_error) {
 //    die("Connection failed: " . $conn->connect_error);
