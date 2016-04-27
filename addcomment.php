17301e8c6adca4bab9c1d5ecb58d4b22a84775d6
Csaba <0811994@rgu.ac.uk>
2016-04-27 01:51:49 +0100

    protection agains SQLi on main login

--- addcomment.php
+++ addcomment.php
@@ -3,9 +3,8 @@ session_start();
 include("connection.php"); //Establishing connection with our database
 include("utilities.php"); //contains the prepared statements
 $msg = ""; //Variable for storing our errors.
-if(isset($_POST["submit"]))
-{
-    $desc="description";
+if(isset($_POST["submit"])) {
+    $desc = "description";
     $desc = $_POST["desc"];
     $photoID = $_POST["photoID"];
     //Sanitising the input
@@ -13,30 +12,26 @@ if(isset($_POST["submit"]))
     $photoID = mysqli_real_escape_string($db, $photoID);
     //getting the userID from session
     $name = $_SESSION["username"];
-    $sql="SELECT userID FROM users WHERE username='$name'";
-    $result=mysqli_query($db,$sql);
-    $row=mysqli_fetch_array($result,MYSQLI_ASSOC);
-    if(mysqli_num_rows($result) == 1) {
+    $sql = "SELECT userID FROM users WHERE username='$name'";
+    $result = mysqli_query($db, $sql);
+    $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+    if (mysqli_num_rows($result) == 1) {
         $id = $row['userID'];
-           }
-    else{
+    } else {
         $msg = "You need to login first";
     }
-}
+
 //Binding the parameter, the statement is contained in utilities.php
-if(!($addcomment->bind_param("sss", $desc, $id, $photoID)))
-{
-    xecho("Binding has failed" . $addcomment->errno . " " . $addcomment->error);
-}
+    if (!($addcomment->bind_param("sss", $desc, $id, $photoID))) {
+        xecho("Binding has failed" . $addcomment->errno . " " . $addcomment->error);
+    }
 //Executing
-if (!$addcomment->execute())
-{
-    xecho("Execute has failed" . $addcomment->errno . " " . $addcomment->error);
+    if (!$addcomment->execute()) {
+        xecho("Execute has failed" . $addcomment->errno . " " . $addcomment->error);
+    } else {
+        $msg = "Comment added successfully";
+    }
+    $addcomment->close();
+    $conn->close();
 }
-else
-{
-    $msg = "Comment added successfully";
-}
-$addcomment->close();
-$conn->close();
 ?>
