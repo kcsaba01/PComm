84776eab798b45b521a5dd0db8d3e0de368a6db4
Csaba <0811994@rgu.ac.uk>
2016-04-30 23:39:33 +0100

    protection agains SQLi on main login

--- removephoto.php
+++ removephoto.php
@@ -1,14 +1,13 @@
 <?php
-session_start();
 include("connection.php"); //Establishing connection with our database
 include("sessioncheck.php");
 include("utilities.php");
-
+session_start();
 
 if(isset($_GET['id']) and is_numeric($_GET['id']))
 {
     $photoID = $_GET['id'];
-    if ($stmt2 = mysqli_prepare($db,"DELETE FROM photos WHERE photoID='?'")) //Preparing the statement
+    if ($stmt2 = mysqli_prepare($db,"DELETE FROM photos WHERE photoID=?")) //Preparing the statement
     {
         mysqli_stmt_bind_param($stmt2, "i", $photoID); //Binding the variables
         if (mysqli_stmt_execute($stmt2))
