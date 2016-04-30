1b7d7f6b18c885595fe55f43b8fd0f1e1e0c3b35
Csaba <0811994@rgu.ac.uk>
2016-04-30 20:26:05 +0100

    protection agains SQLi on main login

--- photo.php
+++ photo.php
@@ -16,12 +16,8 @@
 <h4>Welcome <?php xecho ($login_user);?> <a href="photos.php" style="font-size:18px">Photos</a>||<a href="searchphotos.php" style="font-size:18px">Search</a>||<a href="logout.php" style="font-size:18px">Logout</a></h4>
 <div id="photo">
     <?php
-        if(isset($_GET['id'])){
+        if(isset($_GET['id']) and is_numerci($_GET['id'])){
             $photoID = $_GET['id'];
-            //Sanitising input
-            $photoID = xssafe($photoID);
-            $photoID = mysqli_real_escape_string($db,$photoID);
-
             $photoSql="SELECT * FROM photos WHERE photoID='$photoID'";
             $photoresult=mysqli_query($db,$photoSql) or die(mysqli_error($db));
             if(mysqli_num_rows($photoresult)==1){
