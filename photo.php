922068983db0fefa7f5aa205bc4176cacaaa7fa5
Csaba <0811994@rgu.ac.uk>
2016-04-30 23:09:43 +0100

    protection agains SQLi on main login

--- photo.php
+++ photo.php
@@ -25,10 +25,10 @@
             if((mysqli_num_rows($photoresult)==1))
             {
                 $photoRow = mysqli_fetch_assoc($photoresult);
-                //if (($_SESSION['userid']) != ($photoresult['userID'])) //protection against URL guessing
-                //{
-                 // echo("test");
-                //}
+                if (($_SESSION['userid']) != (strval($photoresult['userID']))) //protection against URL guessing
+                {
+                 echo("test");
+                }
                 echo ("<h1>".gettype($photoRow['userID']). gettype($_SESSION['userid'])."</h1>");
                 echo ("<h1>".xsssafe($photoRow['title'])."</h1>");
                 echo "<h3>".xsssafe($photoRow['postDate'])."</h3>";
